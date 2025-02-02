e06e8ac0d6a4fd35f1345d1da3d5c8eaf1591cb5
Ubuntu <ubuntu@ip-10-0-5-98.eu-west-2.compute.internal>
2025-02-02 19:47:49 +0000

    Commit update to this file

--- provider.tf
+++ provider.tf
@@ -2,12 +2,13 @@ provider "aws" {
   region = var.aws_region
 }
 
+# store the state file after your S3 bucket 
 terraform {
-  required_version = ">= 1.3.0"
   backend "s3" {
     bucket         = "finance-tracker-tfstate"
-    key            = "terraform.tfstate"
+    key            = "finance-tracker/terraform.tfstate"
     region         = "eu-west-2"
     encrypt        = true
+    dynamodb_table = "finance-tracker-locks"
   }
 }
